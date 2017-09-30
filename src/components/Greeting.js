1cd5025203a9a6e3e7c38b1a85fb40bd31f96ebf
Renee Char <renee@Renees-MacBook-Pro.local>
2017-09-30 15:34:44 -0700

    makes greeting fade in

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import './styles/greeting.scss';
 
 class Greeting extends Component {
   render() {
     return (
-      <section id="greeting">
-        <section id="top">
-          <h1 class="animated fadeIn text-center">Aloha!</h1>
-          <h2 class="animated fadeIn text-center">I'm Renee, a web developer with a passion for design and optimization.</h2>
-          <p class="location animated fadeIn text-center">Based in San Francisco, CA</p>
+      <ReactCSSTransitionGroup
+        transitionName="greeting"
+        transitionAppear={true}
+        transitionAppearTimeout={1500}>
+        <section id="greeting">
+          <section id="top">
+            <h1>Aloha!</h1>
+            <h2>I'm Renee, a web developer with a passion for design and optimization.</h2>
+            <p>Based in San Francisco, CA</p>
+          </section>
         </section>
-      </section>
+      </ReactCSSTransitionGroup>
     );
   }
 }
