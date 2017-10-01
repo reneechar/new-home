4b4813e1accc4e0e15a81b7599e5a9e60acea171
Renee Char <renee@Renees-MacBook-Pro.local>
2017-09-30 17:00:49 -0700

    finishes replicating ruby version

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
 import './styles/contact.scss';
+import { FaLinkedinSquare, FaEnvelopeO } from 'react-icons/lib/fa'
+import Gh from 'react-icons/lib/go/mark-github'
+
 
 class Contact extends Component {
   render() {
     return (
       <section id="contact">
 		  <h1>Contact Renee</h1>
-		  <div className="contact-info row">
+		  <div className="contact-info">
 		    <div className="renee-picture"></div>
 		    <div className="social-media">
 		      <ul className="contact-options">
 		        <li>
 		          <h2>
-		            <a href="mailto:renee@reneey.com"><i className="fa fa-envelope-o"></i> renee@reneey.com</a>
+		            <a href="mailto:renee@reneey.com"><FaEnvelopeO /> renee@reneey.com</a>
 		          </h2>
 		        </li>
 		        <li>
 		          <h2>
-		            <a href="https://www.linkedin.com/in/renee-char-42b2b3a0/"><i className="fa fa-linkedin-square"></i> Linked In</a>
+		            <a href="https://www.linkedin.com/in/renee-char-42b2b3a0/"><FaLinkedinSquare /> Linked In</a>
 		          </h2>
 		        </li>
 		        <li>
 		          <h2>
-		            <a href="https://github.com/reneechar/"><i className="fa fa-github-square"></i> GitHub</a>
+		            <a href="https://github.com/reneechar/"><Gh /> GitHub</a>
 		          </h2>
 		        </li>
 		      </ul>
